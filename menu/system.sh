de8568037d4659b46ffbf054b51cf222996c56d3
MuhammadIdsal <81858237+MuhammadIdsal@users.noreply.github.com>
2025-08-29 23:25:42 +0700

    Update system.sh

--- menu/system.sh
+++ menu/system.sh
@@ -532,7 +532,7 @@ echo -e " $COLOR1║${NC} ${WH}[${COLOR1}03${WH}]${NC} ${COLOR1}• ${WH}CHANGE 
 echo -e " $COLOR1║${NC} ${WH}[${COLOR1}04${WH}]${NC} ${COLOR1}• ${WH}CHANGE CORE MENU    ${WH}${WH}[${COLOR1}09${WH}]${NC} ${COLOR1}• ${WH}CHECK PORT VPS     ${WH}$COLOR1║ $NC"
 echo -e " $COLOR1║${NC} ${WH}[${COLOR1}05${WH}]${NC} ${COLOR1}• ${WH}CLEAR RAM CACHE     ${WH}${WH}[${COLOR1}10${WH}]${NC} ${COLOR1}• ${WH}REBUILD VPS        ${WH}$COLOR1║ $NC"
 echo -e " $COLOR1║${NC} ${WH}[${COLOR1}11${WH}]${NC} ${COLOR1}• ${WH}DELETE USER EXPIRED ${WH}${WH}[${COLOR1}0${WH}]${NC} ${COLOR1}• ${WH}EXIT                ${WH}$COLOR1║ $NC"                       
-                                           ${WH}$COLOR1║ $NC"
+                                                                      ${WH}$COLOR1║ $NC"
 echo -e " $COLOR1╚══════════════════════════════════════════════════════╝${NC}"
 echo -e ""
 echo -ne " ${WH}Select menu ${COLOR1}: ${WH}"; read opt
