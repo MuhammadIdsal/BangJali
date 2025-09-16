d2339a1a2995972bad1cb11f5254e5b6fdde286d
MuhammadIdsal <81858237+MuhammadIdsal@users.noreply.github.com>
2025-09-16 19:58:57 +0700

    Update system.sh

--- menu/system.sh
+++ menu/system.sh
@@ -530,8 +530,7 @@ echo -e " $COLOR1║${NC} ${WH}[${COLOR1}02${WH}]${NC} ${COLOR1}• ${WH}CHANGE 
 echo -e " $COLOR1║${NC} ${WH}[${COLOR1}03${WH}]${NC} ${COLOR1}• ${WH}CHANGE THEMA SC     ${WH}${WH}[${COLOR1}08${WH}]${NC} ${COLOR1}• ${WH}CHECK CPU VPS      ${WH}$COLOR1║ $NC"
 echo -e " $COLOR1║${NC} ${WH}[${COLOR1}04${WH}]${NC} ${COLOR1}• ${WH}CHANGE CORE MENU    ${WH}${WH}[${COLOR1}09${WH}]${NC} ${COLOR1}• ${WH}CHECK PORT VPS     ${WH}$COLOR1║ $NC"
 echo -e " $COLOR1║${NC} ${WH}[${COLOR1}05${WH}]${NC} ${COLOR1}• ${WH}CLEAR RAM CACHE     ${WH}${WH}[${COLOR1}10${WH}]${NC} ${COLOR1}• ${WH}REBUILD VPS        ${WH}$COLOR1║ $NC"
-echo -e " $COLOR1║${NC} ${WH}[${COLOR1}11${WH}]${NC} ${COLOR1}• ${WH}DELETE USER EXPIRED ${WH}${WH}[${COLOR1}00${WH}]${NC} ${COLOR1}• ${WH}EXIT               ${WH}$COLOR1║ $NC"
-echo -e " $COLOR1║${NC} ${WH}          [${COLOR1}12${WH}]${NC} ${COLOR1}• ${WH}SPEEDTEST                          ${WH}${WH}${COLOR1} ${WH}$COLOR1║ $NC"
+echo -e " $COLOR1║${NC} ${WH}[${COLOR1}11${WH}]${NC} ${COLOR1}• ${WH}DELETE USER EXPIRED ${WH}${WH}[${COLOR1}12${WH}]${NC} ${COLOR1}• ${WH}SPEED TEST         ${WH}$COLOR1║ $NC"
 echo -e " $COLOR1╚══════════════════════════════════════════════════════╝${NC}"
 echo -e ""
 echo -ne " ${WH}Select menu ${COLOR1}: ${WH}"; read opt
@@ -547,6 +546,7 @@ case $opt in
 09 |9) clear ; check-port ;; 
 10 |10) clear ; wget -q https://raw.githubusercontent.com/MuhammadIdsal/BangJali/raw/refs/heads/main/install-ulang-vps && bash install-ulang-vps ;; 
 11 |11) clear ; del-exp ;;
+12 |12) clear ; del-exp ;;
 00 |0) clear ; menu ;; 
 *) echo -e "" ; echo "Anda salah tekan" ; sleep 1 ; system ;;
 esac
